11bdfc90e6d405caaa8950f7cc0eefbe2759ca87
Miro Georgiev <mgdreams101@gmail.com>
2022-10-25 23:01:29 +0300

    Added tests and constants

--- SampleTestAutomationSeleniumJava/src/Helpers/Constants.java
+++ SampleTestAutomationSeleniumJava/src/Helpers/Constants.java
@@ -2,4 +2,10 @@ public class Constants {
 
     final String DESIRED_WEBSITE = "https://the-internet.herokuapp.com/";
     final String EXPECTED_HOMEPAGE_TITLE = "The Internet";
+    final String DOC_TITLE = "Welcome to the-internet";
+    final String DOC_TITLE_XPATH = "//*[@id=\"content\"]/h1";
+    final String DOC_TITLE_FONTSIZE = "44px";
+    final String DOC_TITLE_COLOR = "rgba(34, 34, 34, 1)";
+    final String DOC_SUBTITLE_XPATH = "//*[@id=\"content\"]/h2";
+    final String DOC_SUBTITLE = "Available Examples";
 }
--- SampleTestAutomationSeleniumJava/src/test/java/DemoAutomation.java
+++ SampleTestAutomationSeleniumJava/src/test/java/DemoAutomation.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -25,5 +27,30 @@ public class DemoAutomation {
         driver.get(constants.DESIRED_WEBSITE);
         String actualTitle = driver.getTitle();
         Assert.assertEquals(constants.EXPECTED_HOMEPAGE_TITLE, actualTitle);
+    }
+
+    @Test
+    public void verifyDocTitle() {
+        String docTitle = driver.findElement(By.xpath(constants.DOC_TITLE_XPATH)).getText();
+        Assert.assertEquals(constants.DOC_TITLE, docTitle);
+    }
+
+    @Test
+    public void verifyDocTitleFontSize(){
+        WebElement docTitle = driver.findElement(By.xpath(constants.DOC_TITLE_XPATH));
+        String docTitleFontSize = docTitle.getCssValue("font-size");
+        Assert.assertEquals(constants.DOC_TITLE_FONTSIZE, docTitleFontSize);
+    }
+
+    @Test
+    public void verifyDocTitleFontColor(){
+        WebElement docTitle = driver.findElement(By.xpath(constants.DOC_TITLE_XPATH));
+        String docTitleColor = docTitle.getCssValue("color");
+        Assert.assertEquals(constants.DOC_TITLE_COLOR, docTitleColor);
+    }
+    @Test
+    public void verifyDocSubTitle(){
+        String docSubTitle = driver.findElement(By.xpath(constants.DOC_SUBTITLE_XPATH)).getText();
+        Assert.assertEquals(docSubTitle, constants.DOC_SUBTITLE);
     }
 }
