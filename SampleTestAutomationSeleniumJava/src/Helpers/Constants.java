af7f7959f6b9098d76066e352c8e9faab18140be
Miro Georgiev <mgdreams101@gmail.com>
2022-10-25 22:55:45 +0300

    Added Constants class in Helpers

---
+++ SampleTestAutomationSeleniumJava/src/Helpers/Constants.java
@@ -0,0 +1,5 @@
+public class Constants {
+
+    final String DESIRED_WEBSITE = "https://the-internet.herokuapp.com/";
+    final String EXPECTED_HOMEPAGE_TITLE = "The Internet";
+}
--- SampleTestAutomationSeleniumJava/src/test/java/DemoAutomation.java
+++ SampleTestAutomationSeleniumJava/src/test/java/DemoAutomation.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class DemoAutomation {
 
     private GetWebDriverAndUse helper = new GetWebDriverAndUse();
-
+    private Constants constants = new Constants();
     public static WebDriver driver;
 
     @BeforeTest
@@ -13,9 +15,15 @@ public class DemoAutomation {
         driver = helper.createCorrectWebDriver("chrome");
         driver.manage().window().maximize();
     }
-
     @AfterTest
     public void closeBrowser(){
         driver.quit();
+    }
+
+    @Test
+    public void testHomePageTitle() {
+        driver.get(constants.DESIRED_WEBSITE);
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(constants.EXPECTED_HOMEPAGE_TITLE, actualTitle);
     }
 }
